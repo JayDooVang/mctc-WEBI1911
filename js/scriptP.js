7a31bf78d6c6bd0e53b84a8262b4119b25dbfe7a
JayDoo Vang <jaydoovang@outlook.com>
2025-04-30 08:21:16 -0500

    adding palindrome HTML & JS

---
+++ js/scriptP.js
@@ -0,0 +1,29 @@
+let textInput = document.getElementById("text-input");
+let checkBtn = document.getElementById("check-btn");
+let result = document.getElementById("result");
+
+checkBtn.addEventListener("click", palindrome);
+
+function palindrome(){
+  if (textInput.value =="") {
+    alert("Please input a value");
+    result.innerHTML = "";
+    return;
+  }
+
+  const processedStr = textInput.value.replace(/[\W_]/g, "").toLowerCase();
+  let isPalindrome = true;
+
+  for (let i =0; i < Math.floor(processedStr.length / 2); i++) {
+    if (processedStr[i] !== processedStr[processedStr.length - 1 - i]){
+      isPalindrome = false;
+      break;
+    }
+  }
+
+  if (isPalindrome){
+    result.innerHTML = textInput.value + " is a palindrome";
+  } else {
+    result.innerHTML = textInput.value + " is not a palindrome";
+  }
+}
